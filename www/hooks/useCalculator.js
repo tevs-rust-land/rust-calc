58b11bfd602079ab915e00d078ff3b00d97033aa
Tevinthuku <tevinthuku@gmail.com>
2021-06-29 00:11:38 +0300

    fix wasm bug

--- www/hooks/useCalculator.js
+++ www/hooks/useCalculator.js
@@ -1,35 +1,39 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export default function useCalculator() {
-  const wasmCalc = useRef(null);
+  const [wasmCalc, setWasmCalc] = useState(null);
   const [expression, setExpression] = useState("");
   const [result, setResult] = useState({
     type: "idle",
   });
 
-  const computeResult = ({ expression }) => {
-    if (!wasmCalc) return { type: "loading" };
-    try {
-      const result = wasmCalc.calculate(expression);
-      return {
-        type: "success",
-        data: result,
-      };
-    } catch (error) {
-      return {
-        type: "error",
-        data: error,
-      };
-    }
-  };
+  const computeResult = useCallback(
+    (expression) => {
+      if (!wasmCalc) return { type: "loading" };
+      try {
+        const result = wasmCalc.calculate(expression);
+        return {
+          type: "success",
+          data: result,
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          type: "error",
+          data: error,
+        };
+      }
+    },
+    [wasmCalc]
+  );
 
   useEffect(() => {
     setResult(computeResult(expression));
-  }, [expression]);
+  }, [computeResult, expression]);
 
   useEffect(() => {
     import("wasm-calc").then((calc) => {
-      wasmCalc.current = calc;
+      setWasmCalc(() => calc);
     });
   }, []);
 
